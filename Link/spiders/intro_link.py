27914d165a49aefe15166e004fbc8badea8b2e9c
Mayurgupta3 <mgupta7042@gmail.com>
2018-07-26 04:04:38 +0530

    Crawler file

---
+++ Link/spiders/intro_link.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class GenSpiderCrawl(CrawlSpider):
+
+	name = 'un_programme_crawler'
+
+	allowed_domains = ['un.org']
+	start_urls = ['http://www.un.org/en/sections/about-un/'\
+					'funds-programmes-specialized-agencies-and-others/index.html']
+
+	rules = (Rule(LinkExtractor(allow= ('funds-programmes-specialized-agencies-and-others'),
+								deny= ('zh/sections', 'fr/sections', 'ru/sections')),
+								 callback = 'parse_page'),)
+
+	def parse_page(self, response):
+
+		list_of_agencies = response.css('div.field-item.even > h4::text').extract()
+
+		for agency in list_of_agencies:
+			with open('un_agencies1.txt', 'a+') as f:
+				f.write(agency + '\n')
